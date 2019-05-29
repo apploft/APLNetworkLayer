f92b103e78ba070cbd1886256d89a22363602938
Ahmet Akbal <ahmet.akbal@apploft.de>
2019-05-29 15:12:35 +0200

    Revert to version 0.0.9

--- Utilities/Classes/NetworkActivitySharedIndicator.swift
+++ Utilities/Classes/NetworkActivitySharedIndicator.swift
@@ -5,32 +5,27 @@
 //  Created by Ahmet Akbal on 27.05.19.
 //  Copyright © 2019 Ahmet Akbal. All rights reserved.
 //
-
 import UIKit
 
-public class NetworkActivitySharedIndicator {
+public class NetworkActivityIndicator {
     
     private static var numberOfActivities = 0
     
-    public static var visible: Bool = false {
+    public static func set(active: Bool) {
         
-        didSet {
-           
-            DispatchQueue.main.async {
-                
-                if visible {
-                    numberOfActivities += 1
-                } else {
-                    numberOfActivities -= 1
-                }
-                
-                if numberOfActivities < 0 {
-                    numberOfActivities = 0
-                }
-                
-                UIApplication.shared.isNetworkActivityIndicatorVisible = (numberOfActivities > 0)
+        DispatchQueue.main.async {
+            
+            if active {
+                numberOfActivities += 1
+            } else {
+                numberOfActivities -= 1
             }
+            
+            if numberOfActivities < 0 {
+                numberOfActivities = 0
+            }
+            
+            UIApplication.shared.isNetworkActivityIndicatorVisible = (numberOfActivities > 0)
         }
     }
-    
 }
